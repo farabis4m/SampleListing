266f3bd21e5ed4c3b8ce0cf68e7cdff6a77cc391
Thahir Maheen <thahir@farabi.ae>
2018-03-12 11:34:04 +0400

    Create Track Model

---
+++ SampleListing/SampleListing/Track.swift
@@ -0,0 +1,16 @@
+//
+//  Track.swift
+//  SampleListing
+//
+//  Created by Farabi Guest on 3/12/18.
+//  Copyright © 2018 Farabi Guest. All rights reserved.
+//
+
+import Foundation
+
+struct Track {
+    var name = ""
+    var author = ""
+    var imageURL = ""
+}
+
