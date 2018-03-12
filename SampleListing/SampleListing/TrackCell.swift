fcd71cb2f917077162b86196ffd1d782bb9be117
Thahir Maheen <thahir@farabi.ae>
2018-03-12 11:46:44 +0400

    Add TrackCell

---
+++ SampleListing/SampleListing/TrackCell.swift
@@ -0,0 +1,31 @@
+//
+//  TrackCell.swift
+//  SampleListing
+//
+//  Created by Farabi Guest on 3/12/18.
+//  Copyright © 2018 Farabi Guest. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+    
+    var track: Track? {
+        didSet {
+            configureView()
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        configureView()
+    }
+    
+    func configureView() {
+        guard let track = track else { return }
+        
+        textLabel?.text = track.name
+        detailTextLabel?.text = track.author
+    }
+}
